fac1d8919a3bb5417136aa6d749861cdd10e5979
Xango2030 <evertoncordeiro@icloud.com>
2023-06-08 18:33:37 -0400

    ajuste no layout

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
     <>
       <Card id='cartao' className=' m-auto mt-5' style={{ width: '40rem' }}>
         <div className='m-auto mt-3'>
-          <img src="https://raw.githubusercontent.com/EvertonCordeiro1994/APIpokemon/main/src/img/Pok%C3%A9dex_logo.png" alt="" />
+          <img className='logo' src="https://raw.githubusercontent.com/EvertonCordeiro1994/APIpokemon/main/src/img/Pok%C3%A9dex_logo.png" alt="" />
         </div>
         
         <CardBody className='m-auto text-center'>
