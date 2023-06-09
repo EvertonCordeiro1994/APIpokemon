2f844de57af48c74dc671f841c76674c9cc9b10c
Xango2030 <evertoncordeiro@icloud.com>
2023-06-08 22:38:06 -0400

    add useREF

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import { Card, CardBody} from 'reactstrap';
+import { Card, CardBody, CardTitle, CardSubtitle, CardText, Button } from 'reactstrap';
 import Info from './components/info.jsx';
 import './style/style.css'
 
 export default function App() {
 
-          
+
   return (
     <>
-      <Card id='cartao' className=' m-auto mt-5' style={{ width: '40rem' }}>
-        <div className='m-auto mt-3'>
-          <img className='logo' src="https://raw.githubusercontent.com/EvertonCordeiro1994/APIpokemon/main/src/img/Pok%C3%A9dex_logo.png" alt="" />
-        </div>
-        
-        <CardBody className='m-auto text-center'>
+      <Card className=' m-auto cartao'>
+        <img style={{ width: '100%', padding: '9px'}}
+          alt="Sample"
+          src="https://raw.githubusercontent.com/EvertonCordeiro1994/APIpokemon/main/src/img/Pok%C3%A9dex_logo.png"
+        />
+        <CardBody>
           <Info />
+
         </CardBody>
       </Card>
     </>
--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,49 +1,67 @@
-import { useState } from 'react'
-import { Button, Input, } from 'reactstrap'
+import { useState, useRef } from 'react'
+import { InputGroup, Button, Input, } from 'reactstrap'
 import axios from 'axios'
 
-export default function PokemonSearch(){
+export default function PokemonSearch() {
     const [searchTerm, setSearchTerm] = useState('')
     const [pokemonData, setPokemonData] = useState(null)
-  
-    const handleInputChange = (event) => {
-      setSearchTerm(event.target.value)
-    }
-  
+    const pageTopRef = useRef(null)
+
+
+
+    const handleInputChange = (event) => setSearchTerm(event.target.value)
+
     const handleSearch = async () => {
-      try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
-        setPokemonData(response.data)
-      } catch (error) {
-        console.error(error)
-        setPokemonData(null)
-      }
+        try {
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`)
+            setPokemonData(response.data)
+        } catch (error) {
+            console.error(error)
+            setPokemonData(null)
+        }
     }
-  
+
+    const upPage = () => pageTopRef.current.scrollIntoView({ behavior: 'smooth' })
+
+    const handleButtonClick = () => {
+        handleSearch()
+        upPage()
+    }
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault()
+            handleButtonClick()
+        }
+    }
+
+
     return (
-      <>
-        <Input type="text" value={searchTerm} onChange={handleInputChange} />
-        <Button onClick={handleSearch}>Search</Button>
-  
-        {pokemonData && (
-          <div>
-            <img src={pokemonData.sprites.front_default}/>
-            <h1>{pokemonData.name}</h1>
-            <h6>ID: {pokemonData.id}</h6>
-            <h2>Types</h2>
-            <ul>
-              {pokemonData.types.map((typeInfo) => (
-                <li key={typeInfo.slot}>{typeInfo.type.name}</li>
-              ))}
-            </ul>
-            <h2>Moves</h2>
-            <ul>
-              {pokemonData.moves.map((moveInfo) => (
-                <li key={moveInfo.slot}>{moveInfo.move.name}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </>
+        <>
+            {pokemonData && (
+                <div ref={pageTopRef}>
+                    <img style={{ width: '350' }} id='foto' src={pokemonData.sprites.front_default} />
+                    <h1>{pokemonData.name}</h1>
+                    <h6>ID: {pokemonData.id}</h6>
+                    <h2>Types</h2>
+                    <ul>
+                        {pokemonData.types.map((typeInfo) => (
+                            <li key={typeInfo.slot}>{typeInfo.type.name}</li>
+                        ))}
+                    </ul>
+                    <h2>Moves</h2>
+                    <ul>
+                        {pokemonData.moves.map((moveInfo) => (
+                            <li key={moveInfo.slot}>{moveInfo.move.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <InputGroup>
+                <Input type="text" value={searchTerm} onChange={handleInputChange} onKeyPress={handleKeyPress} />
+                <Button onClick={handleButtonClick} >Search</Button>
+
+            </InputGroup>
+        </>
     )
-  }
+}
